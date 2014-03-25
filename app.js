c319d100f704140f013a4e57fa380496e9cce071
Darren <darren.royle@oup.com>
2014-03-25 14:15:55 +0000

    Added callback url

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ passport.deserializeUser(function(obj, done) {
 passport.use(new TwitterStrategy({
     consumerKey: TWITTER_CONSUMER_KEY,
     consumerSecret: TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+    callbackURL: "http://isitjesus.com/auth/twitter/callback"
   },
   function(token, tokenSecret, profile, done) {
     // asynchronous verification, for effect...
